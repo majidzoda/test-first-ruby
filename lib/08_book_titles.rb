7d07695023dda3d4664bb179a22b09163f64bf7a
Firdavsii Majidzoda <majidzoda.f@gmail.com>
2017-02-17 21:12:34 -0500

    08 done

--- lib/08_book_titles.rb
+++ lib/08_book_titles.rb
@@ -1,3 +1,20 @@
 class Book
-  # TODO: your code goes here!
+  attr_reader :title
+
+  def title=(title)
+    @title = Book.capitalTitle(title)
+  end
+
+  def self.capitalTitle(t)
+    not_cap_words = ["and", "or", "the", "over", "in", "of", "a", "an"]
+    title_array = []
+    t.capitalize.split.each_with_index do |w, i|
+      if not_cap_words.include?(w)
+        title_array << w
+      else
+        title_array << w[0].upcase+w[1..(w.length)]
+      end
+    end
+    return title_array.join(" ")
+  end
 end
